8d1cdfe367d5176f3d8214c4439ccc51395fceb4
Charline Körner <charline.koerner@gmx.net>
2024-12-11 11:43:41 +0100

    adding airport and flights from students

--- models/staging/staging_airports.sql
+++ models/staging/staging_airports.sql
@@ -1,7 +1,7 @@
 WITH airports_regions_join AS (
     SELECT * 
-    FROM {{source('flights_data', 'airports')}}
-    LEFT JOIN {{source('flights_data', 'regions')}}
+    FROM {{source('staging_flights', 'airports')}}
+    LEFT JOIN {{source('staging_flights', 'regions')}}
     USING (country)
 )
 SELECT * FROM airports_regions_join
