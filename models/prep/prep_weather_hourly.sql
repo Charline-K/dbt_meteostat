8e2f1c552ef7dd234ac02bd30438c8b3206228eb
Charline Körner <charline.koerner@gmx.net>
2024-12-11 14:10:07 +0100

    adding changes weather

--- models/prep/prep_weather_hourly.sql
+++ models/prep/prep_weather_hourly.sql
@@ -8,19 +8,19 @@ add_features AS (
 		, timestamp::DATE AS time -- only time (hours:minutes:seconds) as TIME data type
         , TO_CHAR(timestamp,'HH24:MI') as hour -- time (hours:minutes) as TEXT data type
         , TO_CHAR(timestamp, 'FMmonth') AS month_name -- month name as a text
-        , TO_CHAR(date, 'Day') AS weekday -- weekday name as text        
+        , TO_CHAR(timestamp, 'Day') AS weekday -- weekday name as text        
         , DATE_PART('day', timestamp) AS date_day
-		, DATE_PART('month', date) AS date_month
-		, DATE_PART('year', date) AS date_year
-		, DATE_PART('week', date) AS cw
+		, DATE_PART('month', timestamp) AS date_month
+		, DATE_PART('year', timestamp) AS date_year
+		, DATE_PART('week', timestamp) AS cw
     FROM hourly_data
 ),
 add_more_features AS (
     SELECT *
 		,(CASE 
-			WHEN time BETWEEN TIME'00:00:00' AND TIME'06:00:00' THEN 'night'
-			WHEN time BETWEEN TIME'06:00:00' AND TIME'18:00:00' THEN 'day'
-			WHEN time BETWEEN TIME'18:00:00' AND TIME'00:00:00' THEN 'evening'
+			WHEN time BETWEEN '00:00:00' AND '06:00:00' THEN 'night'
+			WHEN time BETWEEN '06:00:00' AND '18:00:00' THEN 'day'
+			WHEN time BETWEEN '18:00:00' AND '00:00:00' THEN 'evening'
 		END) AS day_part
     FROM add_features
 )
